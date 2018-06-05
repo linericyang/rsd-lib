5f87f22bbeb5e3e767aa483f5d3a33807aaf080f
Lin Yang <lin.a.yang@intel.com>
2018-06-05 16:42:03 -0700

    Modify attach/detach action support for RSD v2.3 node
    Change-Id: I634dd87231d97231395e6ace2fd1cde3c560f9ed

--- rsd_lib/resources/v2_3/node/node.py
+++ rsd_lib/resources/v2_3/node/node.py
@@ -14,32 +14,134 @@
 #    under the License.
 
 from sushy import exceptions
+from sushy.resources import base
 
 from rsd_lib.resources.v2_1.node import node as v2_1_node
+from rsd_lib.resources.v2_3.node import attach_action_info
+from rsd_lib import utils as rsd_lib_utils
+
+
+class AttachEndpointActionField(base.CompositeField):
+    target_uri = base.Field('target', required=True)
+    action_info_path = base.Field('@Redfish.ActionInfo',
+                                  adapter=rsd_lib_utils.get_resource_identity)
+    action_info = None
+
+
+class DetachEndpointActionField(base.CompositeField):
+    target_uri = base.Field('target', required=True)
+    action_info_path = base.Field('@Redfish.ActionInfo',
+                                  adapter=rsd_lib_utils.get_resource_identity)
+    action_info = None
+
+
+class NodeActionsField(v2_1_node.NodeActionsField):
+    attach_endpoint = AttachEndpointActionField('#ComposedNode.AttachResource')
+    detach_endpoint = DetachEndpointActionField('#ComposedNode.DetachResource')
 
 
 class Node(v2_1_node.Node):
 
-    def attach_endpoint(self, endpoint, protocol=None):
+    _actions = NodeActionsField('Actions', required=True)
+
+    def _get_attach_endpoint_action_element(self):
+        attach_endpoint_action = self._actions.attach_endpoint
+        if not attach_endpoint_action:
+            raise exceptions.MissingActionError(
+                action='#ComposedNode.AttachResource',
+                resource=self._path)
+
+        if attach_endpoint_action.action_info is None:
+            attach_endpoint_action.action_info = \
+                attach_action_info.AttachResourceActionInfo(
+                    self._conn, attach_endpoint_action.action_info_path,
+                    redfish_version=self.redfish_version)
+        return attach_endpoint_action
+
+    def get_allowed_attach_endpoints(self):
+        """Get the allowed endpoints for attach action.
+
+        :returns: A set with the allowed attach endpoints.
+        """
+        attach_action = self._get_attach_endpoint_action_element()
+        for i in attach_action.action_info.parameters:
+            if i['name'] == 'Resource':
+                return i['allowable_values']
+        return ()
+
+    def attach_endpoint(self, resource, protocol=None):
         """Attach endpoint from available pool to composed node
 
-        :param endpoint: Link to endpoint to attach.
+        :param resource: Link to endpoint to attach.
         :param protocol: Protocol of the remote drive.
         :raises: InvalidParameterValueError
         """
         attach_action = self._get_attach_endpoint_action_element()
-        valid_endpoints = attach_action.allowed_values
+        valid_endpoints = self.get_allowed_attach_endpoints()
         target_uri = attach_action.target_uri
 
-        if endpoint and endpoint not in valid_endpoints:
+        if resource and resource not in valid_endpoints:
             raise exceptions.InvalidParameterValueError(
-                parameter='endpoint', value=endpoint,
+                parameter='resource', value=resource,
                 valid_values=valid_endpoints)
 
         data = {}
-        if endpoint is not None:
-            data['Resource'] = {'@odata.id': endpoint}
+        if resource is not None:
+            data['Resource'] = {'@odata.id': resource}
         if protocol is not None:
             data['Protocol'] = protocol
 
         self._conn.post(target_uri, data=data)
+
+    def _get_detach_endpoint_action_element(self):
+        detach_endpoint_action = self._actions.detach_endpoint
+        if not detach_endpoint_action:
+            raise exceptions.MissingActionError(
+                action='#ComposedNode.DetachResource',
+                resource=self._path)
+
+        if detach_endpoint_action.action_info is None:
+            detach_endpoint_action.action_info = \
+                attach_action_info.AttachResourceActionInfo(
+                    self._conn, detach_endpoint_action.action_info_path,
+                    redfish_version=self.redfish_version)
+        return detach_endpoint_action
+
+    def get_allowed_detach_endpoints(self):
+        """Get the allowed endpoints for detach action.
+
+        :returns: A set with the allowed detach endpoints.
+        """
+        detach_action = self._get_detach_endpoint_action_element()
+        for i in detach_action.action_info.parameters:
+            if i['name'] == 'Resource':
+                return i['allowable_values']
+        return ()
+
+    def detach_endpoint(self, resource):
+        """Detach endpoint from available pool to composed node
+
+        :param resource: Link to endpoint to detach.
+        :raises: InvalidParameterValueError
+        """
+        detach_action = self._get_detach_endpoint_action_element()
+        valid_endpoints = self.get_allowed_detach_endpoints()
+        target_uri = detach_action.target_uri
+
+        if resource not in valid_endpoints:
+            raise exceptions.InvalidParameterValueError(
+                parameter='resource', value=resource,
+                valid_values=valid_endpoints)
+
+        data = {}
+        if resource is not None:
+            data['Resource'] = {'@odata.id': resource}
+
+        self._conn.post(target_uri, data=data)
+
+    def refresh(self):
+        super(Node, self).refresh()
+        if self._actions.attach_endpoint:
+            self._actions.attach_endpoint.action_info = None
+        if self._actions.detach_endpoint:
+            self._actions.detach_endpoint.action_info = None
--- rsd_lib/tests/unit/resources/v2_3/node/test_node.py
+++ rsd_lib/tests/unit/resources/v2_3/node/test_node.py
@@ -34,25 +34,169 @@ class NodeTestCase(testtools.TestCase):
             self.conn, '/redfish/v1/Nodes/Node1',
             redfish_version='1.0.2')
 
+    def test__get_attach_endpoint_action_element(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        value = self.node_inst._get_attach_endpoint_action_element()
+        self.assertEqual('/redfish/v1/Nodes/Node1/Actions/'
+                         'ComposedNode.AttachResource',
+                         value.target_uri)
+        self.assertEqual('/redfish/v1/Nodes/Node1/Actions/'
+                         'AttachResourceActionInfo',
+                         value.action_info_path)
+        expected = [
+            {
+                "name": "Resource",
+                "required": True,
+                "data_type": "Object",
+                "object_data_type": "#Resource.Resource",
+                "allowable_values": (
+                    "/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1",
+                )
+            },
+            {
+                "name": "Protocol",
+                "required": False,
+                "data_type": "String",
+                "object_data_type": None,
+                "allowable_values": ["NVMeOverFabrics"]
+            }
+        ]
+        self.assertEqual(expected, value.action_info.parameters)
+
+    def test_get_allowed_attach_endpoints(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        expected = self.node_inst.get_allowed_attach_endpoints()
+        result = ("/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1",)
+        self.assertEqual(expected, result)
+
     def test_attach_endpoint(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
         self.node_inst.attach_endpoint(
-            endpoint='/redfish/v1/StorageServices/NVMeoE1/Volumes/1',
+            resource='/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1',
             protocol='NVMeOverFabrics')
         self.node_inst._conn.post.assert_called_once_with(
-            '/redfish/v1/Nodes/Node1/Actions/ComposedNode.AttachEndpoint',
-            data={'Resource': {'@odata.id': '/redfish/v1/'
-                               'StorageServices/NVMeoE1/Volumes/1'},
+            '/redfish/v1/Nodes/Node1/Actions/ComposedNode.AttachResource',
+            data={'Resource': {'@odata.id': '/redfish/v1/StorageServices'
+                                            '/1-sv-1/Volumes/1-sv-1-vl-1'},
                   'Protocol': 'NVMeOverFabrics'})
 
-    def test_attach_endpoint_invalid_parameter(self):
-        self.assertRaises(exceptions.InvalidParameterValueError,
-                          self.node_inst.attach_endpoint,
-                          endpoint='invalid')
-
     def test_attach_endpoint_only_with_resource_uri(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
         self.node_inst.attach_endpoint(
-            endpoint='/redfish/v1/StorageServices/NVMeoE1/Volumes/1')
+            resource='/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1')
         self.node_inst._conn.post.assert_called_once_with(
-            '/redfish/v1/Nodes/Node1/Actions/ComposedNode.AttachEndpoint',
-            data={'Resource': {'@odata.id': '/redfish/v1/'
-                               'StorageServices/NVMeoE1/Volumes/1'}})
+            '/redfish/v1/Nodes/Node1/Actions/ComposedNode.AttachResource',
+            data={'Resource': {'@odata.id': '/redfish/v1/StorageServices'
+                                            '/1-sv-1/Volumes/1-sv-1-vl-1'}})
+
+    def test_attach_endpoint_invalid_parameter(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        with self.assertRaisesRegex(
+            exceptions.InvalidParameterValueError,
+            '"resource" value.*{0}'.format(
+                '/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1')):
+
+            self.node_inst.attach_endpoint(resource='invalid-resource')
+
+    def test__get_detach_endpoint_action_element(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        value = self.node_inst._get_detach_endpoint_action_element()
+        self.assertEqual('/redfish/v1/Nodes/Node1/Actions/'
+                         'ComposedNode.DetachResource',
+                         value.target_uri)
+        self.assertEqual('/redfish/v1/Nodes/Node1/Actions/'
+                         'DetachResourceActionInfo',
+                         value.action_info_path)
+        expected = [
+            {
+                "name": "Resource",
+                "required": True,
+                "data_type": "Object",
+                "object_data_type": "#Resource.Resource",
+                "allowable_values": (
+                    "/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1",
+                )
+            },
+            {
+                "name": "Protocol",
+                "required": False,
+                "data_type": "String",
+                "object_data_type": None,
+                "allowable_values": ["NVMeOverFabrics"]
+            }
+        ]
+        self.assertEqual(expected, value.action_info.parameters)
+
+    def test_get_allowed_detach_endpoints(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        expected = self.node_inst.get_allowed_detach_endpoints()
+        result = ("/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1",)
+        self.assertEqual(expected, result)
+
+    def test_detach_endpoint(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        self.node_inst.detach_endpoint(
+            resource='/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1')
+        self.node_inst._conn.post.assert_called_once_with(
+            '/redfish/v1/Nodes/Node1/Actions/ComposedNode.DetachResource',
+            data={'Resource': {'@odata.id': '/redfish/v1/StorageServices'
+                                            '/1-sv-1/Volumes/1-sv-1-vl-1'}})
+
+    def test_detach_endpoint_invalid_parameter(self):
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        with self.assertRaisesRegex(
+            exceptions.InvalidParameterValueError,
+            '"resource" value.*{0}'.format(
+                '/redfish/v1/StorageServices/1-sv-1/Volumes/1-sv-1-vl-1')):
+
+            self.node_inst.detach_endpoint(resource='invalid-resource')
+
+    def test_refresh(self):
+        self.assertIsNone(self.node_inst._actions.attach_endpoint.action_info)
+        self.assertIsNone(self.node_inst._actions.detach_endpoint.action_info)
+
+        with open('rsd_lib/tests/unit/json_samples/v2_3/'
+                  'attach_action_info.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+
+        self.node_inst._get_attach_endpoint_action_element()
+        self.node_inst._get_detach_endpoint_action_element()
+
+        self.assertIsNotNone(
+            self.node_inst._actions.attach_endpoint.action_info)
+        self.assertIsNotNone(
+            self.node_inst._actions.detach_endpoint.action_info)
+
+        with open('rsd_lib/tests/unit/json_samples/v2_3/node.json', 'r') as f:
+            self.conn.get.return_value.json.return_value = json.loads(f.read())
+        self.node_inst.refresh()
+
+        self.assertIsNone(self.node_inst._actions.attach_endpoint.action_info)
+        self.assertIsNone(self.node_inst._actions.detach_endpoint.action_info)
